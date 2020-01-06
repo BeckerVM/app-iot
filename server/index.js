3ec0048e97f89288159bf1e3f984a5fdc5cd4292
unknown <beiker2203@gmail.com>
2020-01-06 18:23:54 -0500

    Realinzando la conexion con el cliente socket

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
+const { Board, Led } = require('johnny-five')
 
 const app = express()
 
@@ -14,6 +15,32 @@ app.use('/api/device', require('./routes/device'))
 
 const PORT = 5000
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log('Server starting in port ' + PORT)
+})
+
+const io = require('./socket').init(server)
+
+//ARDUINO VARIABLES
+let myBoard = null
+let devices = []
+
+io.on('connection', (socket) => {
+  console.log('Connected platform')
+
+  myBoard = new Board()
+
+  myBoard.on('ready', function() {
+    console.log('ready')
+    socket.emit('arduino-connected', true)
+  })
+
+  myBoard.on('close', function() {
+    console.log('close')
+    socket.emit('arduino-connected', false)
+  })
+
+  socket.on('disconnect', function() {
+    console.log('Disconnected platform')
+  })
 })
